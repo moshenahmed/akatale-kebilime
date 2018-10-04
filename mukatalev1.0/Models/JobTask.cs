e04922c4bcbdbfba6fd701afd689854885e513e5
Moshen Ahmed Bukenya <moshenahmed@gmail.com>
2018-10-04 15:52:28 +0200

    closing up

---
+++ mukatalev1.0/Models/JobTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace mukatalev1._0.Models
+{
+    public class JobTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public  string JobDescription { get; set; }
+        public string Image { get; set; }
+        public int MainBid { get; set; }
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ mukatalev1.0/Models/UserBid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mukatalev1._0.Models
+{
+    public class UserBid
+    {
+        public int Id { get; set; }
+        public int MyBid { get; set; }
+        public int JobTaskId { get; set; }
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser  User { get; set; }
+        public virtual JobTask JobTask { get; set; }
+    }
+}
